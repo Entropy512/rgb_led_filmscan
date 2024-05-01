d06d213731bd947c3efd227717746707b65b628e
Andy Dodd <atd7@cornell.edu>
2024-05-01 10:08:06 -0400

    Density plotter:  Support varying inref values, add Kodak Gold 200 model values
    FIXME:  Get the film name from the command line.
    
    TODO : Find a way to try and autofit a model

--- density_plot.py
+++ density_plot.py
@@ -54,14 +54,10 @@ density_data['Red'] -= density_data['Red'].min()
 density_data['Green'] -= density_data['Green'].min()
 density_data['Blue'] -= density_data['Blue'].min()
 
+density_data.rename(columns={'Red' : 'r', 'Green' : 'g', 'Blue' : 'b'}, inplace=True)
 
-plt.figure()
-plt.semilogx(np.power(10.0,-density_data['Red']), np.log2(np.power(10,density_data.index)), color='r', alpha=0.5, label='Film Red')
-plt.semilogx(np.power(10.0,-density_data['Green']), np.log2(np.power(10,density_data.index)), color='g', alpha=0.5, label='Film Green')
-plt.semilogx(np.power(10.0,-density_data['Blue']), np.log2(np.power(10,density_data.index)), color='b', alpha=0.5, label='Film Blue')
-
-def tcoeff_to_scenelin(tcoeff, exp, linadj, strexp):
-    scenelin = np.power(tcoeff, -exp)/np.power(10.0,2.8)
+def tcoeff_to_scenelin(tcoeff, reflevel, exp, linadj, strexp):
+    scenelin = np.power(tcoeff, -exp)*reflevel
     return np.power((np.power(scenelin,strexp)-linadj)/(1-linadj), 1.0/strexp)
 
 """ 
@@ -81,7 +77,7 @@ soln = minimize_scalar(equations, bounds=bounds, args=(np.power(10,-3.62), strex
 print(soln)
 """
 
-filmdata = {'Fuji Superia X-Tra 400':   {'inref' : np.power(10.0,-2.8),
+filmdata =  {'Fuji Superia X-Tra 400':   {'inref' : np.power(2.0,-9.3014),
                                         'evdelt' : 2.724,
                                         'exp' : {'r' : 1.6,
                                                 'g' : 1.48,
@@ -89,31 +85,45 @@ filmdata = {'Fuji Superia X-Tra 400':   {'inref' : np.power(10.0,-2.8),
                                         'cstr': {'r' : 1.2,
                                                 'g' : 2.0,
                                                 'b' : 2.6}
-                                        }}
+                                        },
+            'Kodak Gold 200':   {'inref' : np.power(2.0,-7.95),
+                                        'evdelt' : 4.0,
+                                        'exp' : {'r' : 1.83,
+                                                'g' : 1.72,
+                                                'b' : 1.65},
+                                        'cstr': {'r' : 2.0,
+                                                'g' : 1.8,
+                                                'b' : 3.0}
+                                        }
+            }
 
 density_vals = np.linspace(0,2.5,2000)
 tcoeff_vals = np.power(10,-(density_vals))
 
-film = 'Fuji Superia X-Tra 400'
+film = 'Kodak Gold 200'
 inref = filmdata[film]['inref']
 evdelt = filmdata[film]['evdelt']
 outref = inref*np.power(2.0,-evdelt)
+plotnum = 0
+pltn = None
 
+fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
+axs[-1,-1].axis('off')
+fig.suptitle('Scene Light vs. Film Transmission Coefficient for' + film)
 for color in ['r', 'g', 'b']:
     exp = filmdata[film]['exp'][color]
     cstr = filmdata[film]['cstr'][color]
     linadj = (np.power(outref,cstr)-np.power(inref,cstr))/(np.power(outref,cstr)-1)
-    plt.semilogx(np.power(10,-density_vals), np.log2(tcoeff_to_scenelin(tcoeff_vals, exp, 0.0, 1.0)), color=color, dashes=[1,3], label='Simple Exponent (exp = -{})'.format(exp))
-    plt.semilogx(np.power(10,-density_vals), np.log2(tcoeff_to_scenelin(tcoeff_vals, exp, linadj, cstr)), color=color, dashes=[2,1], label='Enhanced Model (exp = -{}, str={})'.format(exp,cstr))
+    pltn = axs[plotnum % 2, plotnum // 2]
+    plotnum += 1
 
-exp = 1.6
-strexp = 1.2
+    pltn.semilogx(np.power(10.0,-density_data[color]), np.log2(np.power(10,density_data.index)), color=color, alpha=0.5, label='Film Response')
+    pltn.semilogx(np.power(10,-density_vals), np.log2(tcoeff_to_scenelin(tcoeff_vals, inref, exp, 0.0, 1.0)), color=color, dashes=[1,3], label='Simple Exponent (exp = -{})'.format(exp))
+    pltn.semilogx(np.power(10,-density_vals), np.log2(tcoeff_to_scenelin(tcoeff_vals, inref, exp, linadj, cstr)), color=color, dashes=[2,1], label='Enhanced Model (exp = -{}, str={})'.format(exp,cstr))
+    pltn.set_xlabel('Normalized Transmission Coefficient\n(Orange Mask = 1.0)')
+    pltn.set_ylabel('Scene Light (EV)')
+    pltn.legend()
 
 
-
-plt.xlabel('Normalized Transmission Coefficient (Orange Mask = 1.0)')
-plt.ylabel('Scene Light (EV)')
-plt.title('Scene Light vs. Film Transmission Coefficient for' + film)
-plt.legend()
 plt.show()
 
