45d95087c44e596a5f7d97cacfbd53677db6fc6e
Andy Dodd <atd7@cornell.edu>
2024-05-07 16:45:22 -0400

    Density analysis:
    Pivot our dataframe so we have one that gives scene exposure vs. density with density as the index
    
    Update plots accordingly, also plot vs. density instead of semilogx of transmission coefficient

--- density_plot.py
+++ density_plot.py
@@ -39,22 +39,36 @@ density_data = pd.concat(datasets.values()).sort_index()
 # https://stackoverflow.com/questions/35855772/pandas-merge-duplicate-index-into-single-index
 density_data = density_data.groupby(density_data.index).sum(min_count=1)
 
+
+density_data.rename(columns={'Red' : 'r', 'Green' : 'g', 'Blue' : 'b'}, inplace=True)
+
+density_data.r -= density_data.r.min()
+density_data.g -= density_data.g.min()
+density_data.b -= density_data.b.min()
+
+# https://stackoverflow.com/questions/43781465/how-to-swap-index-and-values-on-pandas-dataframe
+scene_data = density_data.stack().reset_index()
+scene_data.columns = ['i', 'c', 'd']
+
+scene_data = scene_data.pivot_table(index='d', columns='c', values='i')
+
 # https://stackoverflow.com/questions/41493282/in-python-pandas-how-can-i-re-sample-and-interpolate-a-dataframe
-dstep = 0.1
-exposure_vals = np.arange(-3.62,1.0 + dstep, dstep)
+istep = 0.1
+exposure_vals = np.arange(-3.62,1.0 + istep, istep)
 density_data = density_data.reindex(density_data.index.union(exposure_vals))
+
+dstep = 0.01
+density_vals = np.arange(0, 2.5 + dstep, dstep)
+scene_data = scene_data.reindex(scene_data.index.union(density_vals))
 
 #TODO:  Determine if we need to bother with extrapolation and interpolation since we're just curve-fitting.  May cause more problems than it solves in this application.  The dangers of copypasta!
 # https://stackoverflow.com/questions/68548971/linearly-extrapolate-pandas-dataframe-using-built-in-interpolate-method
 density_data.interpolate(method='slinear', fill_value="extrapolate", limit_direction="both", inplace=True)
-
 density_data = density_data.loc[exposure_vals]
 
-density_data['Red'] -= density_data['Red'].min()
-density_data['Green'] -= density_data['Green'].min()
-density_data['Blue'] -= density_data['Blue'].min()
+scene_data.interpolate(method='slinear', fill_value="extrapolate", limit_direction="both", inplace=True)
+scene_data = scene_data.loc[density_vals]
 
-density_data.rename(columns={'Red' : 'r', 'Green' : 'g', 'Blue' : 'b'}, inplace=True)
 
 def tcoeff_to_scenelin(tcoeff, reflevel, exp, linadj, strexp):
     scenelin = np.power(tcoeff, -exp)*reflevel
@@ -109,7 +123,7 @@ pltn = None
 
 fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
 axs[-1,-1].axis('off')
-fig.suptitle('Scene Light vs. Film Transmission Coefficient for' + film)
+fig.suptitle('Scene Light vs. Film Transmission Coefficient for ' + film)
 for color in ['r', 'g', 'b']:
     exp = filmdata[film]['exp'][color]
     cstr = filmdata[film]['cstr'][color]
@@ -117,10 +131,11 @@ for color in ['r', 'g', 'b']:
     pltn = axs[plotnum % 2, plotnum // 2]
     plotnum += 1
 
-    pltn.semilogx(np.power(10.0,-density_data[color]), np.log2(np.power(10,density_data.index)), color=color, alpha=0.5, label='Film Response')
-    pltn.semilogx(np.power(10,-density_vals), np.log2(tcoeff_to_scenelin(tcoeff_vals, inref, exp, 0.0, 1.0)), color=color, dashes=[1,3], label='Simple Exponent (exp = -{})'.format(exp))
-    pltn.semilogx(np.power(10,-density_vals), np.log2(tcoeff_to_scenelin(tcoeff_vals, inref, exp, linadj, cstr)), color=color, dashes=[2,1], label='Enhanced Model (exp = -{}, str={})'.format(exp,cstr))
-    pltn.set_xlabel('Normalized Transmission Coefficient\n(Orange Mask = 1.0)')
+    pltn.xaxis.set_tick_params(labelbottom=True)
+    pltn.plot(scene_data.index, np.log2(np.power(10,scene_data[color])), color=color, alpha=0.5, label='Film Response')
+    pltn.plot(density_vals, np.log2(tcoeff_to_scenelin(tcoeff_vals, inref, exp, 0.0, 1.0)), color=color, dashes=[1,3], label='Simple Exponent (exp = -{})'.format(exp))
+    pltn.plot(density_vals, np.log2(tcoeff_to_scenelin(tcoeff_vals, inref, exp, linadj, cstr)), color=color, dashes=[2,1], label='Enhanced Model (exp = -{}, str={})'.format(exp,cstr))
+    pltn.set_xlabel('Density difference from Dmin')
     pltn.set_ylabel('Scene Light (EV)')
     pltn.legend()
 
